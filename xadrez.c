92a5f493fdc9b8b3fbe1c33ec0e33202eafe712d
Eduardo Alves <102545171+Duzzyn@users.noreply.github.com>
2025-10-28 17:22:28 -0300

    Mestre xadrez.x

--- xadrez.c
+++ xadrez.c
@@ -1,75 +1,97 @@
 #include <stdio.h>
 
+// Movimento da torre
+void torre(int casas) {
+    if (casas > 0) {
+        printf("DIREITA!\n");
+        torre(casas - 1);
+    }
+    
+}
+// Movimento do bispo
+void bispo(int casas) {
+    if (casas > 0) {
+        for (int p = 0; p < 1; p++) {
+            printf("CIMA, ");
+        }
+        for (int i = 0; i < 1; i++) {
+            printf("DIREITA!\n");
+        }
+        bispo(casas - 1);  
+             
+    }
+    
+}
+// Movimento da rainha
+void rainha(int casas) {
+    if (casas > 0) {
+        printf("ESQUERDA!\n");
+        rainha(casas - 1);
+    }
+    
+}
+// Movimento do cavalo
+void cavalo() {
+    int mcima = 2;
+    int mdireita = 1;
+
+    for (int i = 0; i < mcima + mdireita; i++) {
+        if (i < mcima) {
+            printf("Cima\n");
+            continue;
+        }
+        if (i == mcima) {
+            printf("Direita!\n");
+            break;
+        }
+    }
+    
+}
+
+
 int main() {
 
-   int torre = 5;
-   int bispo = 5;
-   int rainha = 8;
-   int cavalo = 1
-
-   int opcoes;
+    int opcoes;
    
     do {
-        printf("**Peças Disponiveis**\n");
-        printf("ESCOLHA UMA PEÇA PARA QUE SEJA DEMONSTRADO OS SEUS MOVIMENTOS!\n"); // Usuário precisa escolher uma peça
-        printf("1. Torre\n");
-        printf("2. Bispo\n");
-        printf("3. Rainha\n");
-        printf("4. Cavalo\n");
-        printf("5. Sair\n");
+        printf("**MENU PEÇAS DE XADREZ**\n");
+        printf("ESCOLHA UMA PEÇA PARA QUE SEJA DEMONSTRADO OS SEUS MOVIMENTOS!\n");
+        printf("1. TORRE\n");
+        printf("2. BISPO\n");
+        printf("3. CAVALO\n");
+        printf("4. RAINHA\n");
+        printf("5. SAIR!\n");
         printf("ESCOLHA UMA OPÇÃO: \n");
         scanf("%d", &opcoes);
        
         switch(opcoes) {
             case 1:
-                printf("*---MOVIMENTOS DA PEÇA TORRE---*\n");//Movimento da torre
-                int i = 1;
-                while (i <= TORRE)
-                {
-                   printf("DIREITA!\n");
-                   i++;
-                }
+                printf("*--Movimentos da Torre--*\n"); 
+                torre(5);
                 printf("\n");
                 break;
             case 2:
-                printf("*---MOVIMENTOS DA PEÇA BISPO---*\n");//LOGICA PARA O BISPO
-                int p = 1;
-                while (p <= BISPO)
-                {
-                   printf("CIMA, DIREITA!\n");
-                   p++;
-                }
+                printf("*--Movimentos do Bispo--*\n"); 
+                bispo(5);
                 printf("\n");
                 break;
             case 3:
-                printf("*---MOVIMENTOS DA PEÇA RAINHA---*\n");//LOGICA PARA A RAINHA
-                int r = 1;
-                while (r <= RAINHA)
-                {
-                   printf("ESQUERDA!\n");
-                   r++;
-                }
+                printf("*--Movimentos do Cavalo--*\n");
+                cavalo();
                 printf("\n");
                 break;
             case 4:
-                printf("*---MOVIMENTOS DA PEÇA CAVALO---*\n");
-                while (CAVALO--)
-                {
-                    for(int c = 0; c < 2; c++) {
-                   printf("BAIXO!\n"); //IMPRIME "CIMA" DUAS VEZES
-                   printf("\n");
-                    }
-                printf("DIREITA!\n"); //IMPRIME "DIREITA" UMA VEZ
+                printf("*--Movimentos da Rainha--*\n");
+                rainha(8);
                 printf("\n");
-                }
-                break;  
+                break;
             case 5:
                 printf("Saindo...\n");
                 break;
             default:
                 printf("Opção inválida!\n");
         }
-    } while (option != 5);
+    } while (opcoes != 5);
    
     return 0;
 }
